daf66d195540c5ee455302cf7490953d8877665d
ollivierred <ollivierre.dev@gmail.com>
2017-09-21 01:21:19 -0500

    Added checkbox with the text of confirmed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('registrar');  //Access and stores the form reference.
 const input = form.querySelector('input');  //Access and stores input element reference in form.
+const ul = document.getElementById('invitedList');  //Access and stores 'ul' reference.
+
 
 form.addEventListener('submit', (e) => {  //Adds an event listener to the form's submit event.
   e.preventDefault();  //Cancels the browser's default submit behavior.
@@ -8,9 +10,16 @@ form.addEventListener('submit', (e) => {  //Adds an event listener to the form's
   //append list item to the 'ul'
   const text = input.value;  //Stores the input value
   input.value = '';  //Sets form input to an empty string
-  const ul = document.getElementById('invitedList');  //Access and stores 'ul' reference.
+
   const li = document.createElement('li');  //Create list item element
+  const label = document.createElement('label');  //create label for checkbox element
+  const checkbox = document.createElement('input');  //create checkbox element.
+
   li.textContent = text;  //Puts input value into the list item.
+  label.textContent = "Confirmed";  //
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
   ul.appendChild(li);  //Adds the 'li' to the 'ul'
 
 
